86e486a5bfdb6f8985d5e243ce0b5b73507eb55e
HafizhM <hafizhmuhammad475@gmail.com>
2022-08-23 08:20:55 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -16,28 +16,28 @@ const addBookHandler = (request, h) => {
         name, year, author, summary, publisher, pageCount, readPage, reading, id, finished, insertedAt, updatedAt,
       };
 
+      if(name === undefined) {
+        const response = h.response({
+          status: 'fail',
+          message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+      }
+  
+      if(readPage > pageCount) {
+        const response = h.response({
+          status: 'fail',
+          message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+      }
+
     books.push(newBook);
 
     const isSuccess = books.filter((book) => book.id === id).length > 0;
 
-    if(name === undefined) {
-      const response = h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. Mohon isi nama buku',
-      });
-      response.code(400);
-      return response;
-    }
-
-    if(readPage > pageCount) {
-      const response = h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-      });
-      response.code(400);
-      return response;
-    }
-  
     if(isSuccess) {
         const response = h.response({
             status: 'success',
@@ -147,22 +147,21 @@ const editBookByIdHandler = (request, h) => {
         updatedAt,
       };
 
-      const response = h.response({
+      //const response = h.response({
+      //  status: 'success',
+      //  message: 'Buku berhasil diperbarui',
+      //});
+
+
+      //response.code(200);
+      //return response;
+
+      return {
         status: 'success',
         message: 'Buku berhasil diperbarui',
-      });
-      response.code(200);
-      return response;
+      };
     }
 
-     // if(id === undefined) {
-       // const response = h.response({
-         // status: 'fail',
-         // message: 'Gagal memperbarui buku. Id tidak ditemukan',
-       // });
-       // response.code(404);
-       // return response;
-     // }
         const response = h.response({
             status: 'fail',
             message: 'Gagal memperbarui buku. Id tidak ditemukan',
